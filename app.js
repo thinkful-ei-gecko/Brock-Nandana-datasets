55cdac13b8f92ae2b476459661731cdb9432451e
nrrao <nandanarao@yahoo.com>
2019-09-24 16:44:36 -0400

    finished query

--- app.js
+++ app.js
@@ -23,11 +23,17 @@ app.get('/movie', function handleMovieType(req, res) {
   let response = data;
 
   if (req.query.genre) {
-    response = response.filter(movie => {
-      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
-    });
+    response = response.filter(movie => 
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
   }
-
+  if (req.query.country) {
+    response = response.filter(movie => 
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
+  }
+  if(req.query.avg_vote){
+    response = response.filter(movie =>
+      (movie.avg_vote)>=(req.query.avg_vote));
+  }
 
   res.json(response);
 });
